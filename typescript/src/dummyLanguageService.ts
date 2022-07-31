d3e577a5ec8a489422f0d8a540cd4891ed542b8f
Vitaly Turovsky <vital2580@icloud.com>
2022-07-31 09:46:39 +0300

    tests: make testing env more real. enable latest lib dom in tests

--- typescript/src/dummyLanguageService.ts
+++ typescript/src/dummyLanguageService.ts
@@ -1,20 +1,28 @@
 // only for basic testing, as vscode is actually using server
 import ts from 'typescript/lib/tsserverlibrary'
+import path from 'path'
+import fs from 'fs'
 
 export const createLanguageService = (files: Record<string, string>) => {
     let dummyVersion = 1
+    let defaultLibDir: string | undefined
     const languageService = ts.createLanguageService({
         getProjectVersion: () => dummyVersion.toString(),
         getScriptVersion: () => dummyVersion.toString(),
         getCompilationSettings: () => ({ allowJs: true, jsx: ts.JsxEmit.Preserve, target: ts.ScriptTarget.ESNext }),
         getScriptFileNames: () => Object.keys(files),
         getScriptSnapshot: fileName => {
-            const contents = files[fileName]
+            let contents = files[fileName]
+            if (path.dirname(fileName) === defaultLibDir) contents = ts.sys.readFile(fileName)
             if (contents === undefined) return
             return ts.ScriptSnapshot.fromString(contents)
         },
         getCurrentDirectory: () => '',
-        getDefaultLibFileName: () => require.resolve('typescript/lib/lib.esnext.full.d.ts'),
+        getDefaultLibFileName: options => {
+            const defaultLibPath = ts.getDefaultLibFilePath(options)
+            defaultLibDir = path.dirname(defaultLibPath)
+            return defaultLibPath
+        },
     })
     return {
         languageService,
