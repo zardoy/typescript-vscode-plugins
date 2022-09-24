ddcb20551573590d0d779537f6f963fef4bbf750
Vitaly Turovsky <vital2580@icloud.com>
2022-09-25 00:48:50 +0300

    refactor array methods snippets

--- typescript/src/completions/arrayMethods.ts
+++ typescript/src/completions/arrayMethods.ts
@@ -2,44 +2,45 @@ import { GetConfig } from '../types'
 import { findChildContainingPosition, getLineTextBeforePos } from '../utils'
 import { singular } from 'pluralize'
 
-export default (entries: ts.CompletionEntry[], _node: ts.Node | undefined, position: number, sourceFile: ts.SourceFile, c: GetConfig): ts.CompletionEntry[] => {
-    if (!c('arrayMethodsSnippets.enable')) return entries
+const arrayMethodsToPatch = [
+    'forEach',
+    'map',
+    'flatMap',
+    'filter',
+    'find',
+    'findIndex',
+    //  'reduce',
+    //  'reduceRight',
+    'some',
+    'every',
+]
+
+export default (entries: ts.CompletionEntry[], position: number, sourceFile: ts.SourceFile, c: GetConfig): ts.CompletionEntry[] | undefined => {
+    if (!c('arrayMethodsSnippets.enable')) return
     /** Methods to patch */
-    const arrayMethods = [
-        'forEach',
-        'map',
-        'flatMap',
-        'filter',
-        'find',
-        'findIndex',
-        //  'reduce',
-        //  'reduceRight',
-        'some',
-        'every',
-    ]
+
     const fullText = sourceFile.getText()
-    if (fullText.slice(position, position + 1) === '(') return entries
-    const isSeemsArray = arrayMethods.every(comparingName =>
-        entries.some(
-            ({ name, isSnippet, kind }) => name.replace(/^★ /, '') === comparingName && !isSnippet && kind === ts.ScriptElementKind.memberFunctionElement,
-        ),
-    )
-    if (!isSeemsArray) return entries
+    if (fullText.slice(position, position + 1) === '(') return
+    const seemsArray = isArrayLike(entries)
+    if (!seemsArray) return
+
     const lineTextBefore = getLineTextBeforePos(sourceFile, position)
     const postfixRemoveLength = /\.\w*$/.exec(lineTextBefore)?.[0]?.length
-    if (postfixRemoveLength === undefined) return entries
+    if (postfixRemoveLength === undefined) return
     const nodeBeforeDot = findChildContainingPosition(ts, sourceFile, position - postfixRemoveLength - 1)
-    if (!nodeBeforeDot) return entries
+    if (!nodeBeforeDot) return
+
     const cleanSourceText = getItemNameFromNode(nodeBeforeDot)?.replace(/^(?:all)?(.+?)(?:List)?$/, '$1')
     let inferredName = cleanSourceText && singular(cleanSourceText)
     const defaultItemName = c('arrayMethodsSnippets.defaultItemName')
     // both can be undefined
     if (inferredName === cleanSourceText) {
-        if (defaultItemName === false) return entries
+        if (defaultItemName === false) return
         inferredName = defaultItemName
     }
+
     return entries.map(entry => {
-        if (!arrayMethods.includes(entry.name.replace(/^★ /, ''))) return entry
+        if (!arrayMethodsToPatch.includes(entry.name.replace(/^★ /, ''))) return entry
         const arrayItemSnippet = c('arrayMethodsSnippets.addArgTabStop') ? `(\${2:${inferredName}})` : inferredName
         let insertInnerSnippet = `${arrayItemSnippet} => $3`
         if (c('arrayMethodsSnippets.addOuterTabStop')) insertInnerSnippet = `\${1:${insertInnerSnippet}}`
@@ -61,4 +62,12 @@ const getItemNameFromNode = (node: ts.Node) => {
         }
     }
     return undefined
+}
+
+export const isArrayLike = (entries: ts.CompletionEntry[]) => {
+    return arrayMethodsToPatch.every(comparingName =>
+        entries.some(
+            ({ name, isSnippet, kind }) => name.replace(/^★ /, '') === comparingName && !isSnippet && kind === ts.ScriptElementKind.memberFunctionElement,
+        ),
+    )
 }
