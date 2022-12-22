98df79afc85232b6cdf82970e7134ce746258f51
Vitaly Turovsky <vital2580@icloud.com>
2022-12-22 16:31:17 +0300

    fix lint regression

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,5 @@
-import { offsetPosition } from '@zardoy/vscode-utils/build/position'
-import ts from 'typescript'
 import * as vscode from 'vscode'
+import { offsetPosition } from '@zardoy/vscode-utils/build/position'
 
 export const tsRangeToVscode = (document: vscode.TextDocument, [start, end]: [number, number]) =>
     new vscode.Range(document.positionAt(start), document.positionAt(end))
@@ -8,12 +7,11 @@ export const tsRangeToVscode = (document: vscode.TextDocument, [start, end]: [nu
 export const tsRangeToVscodeSelection = (document: vscode.TextDocument, [start, end]: [number, number]) =>
     new vscode.Selection(document.positionAt(start), document.positionAt(end))
 
-export const tsTextChangesToVcodeTextEdits = (document: vscode.TextDocument, edits: ts.TextChange[]): vscode.TextEdit[] => {
-    return edits.map(({ span, newText }) => {
+export const tsTextChangesToVcodeTextEdits = (document: vscode.TextDocument, edits: Array<import('typescript').TextChange>): vscode.TextEdit[] =>
+    edits.map(({ span, newText }) => {
         const start = document.positionAt(span.start)
         return {
             range: new vscode.Range(start, offsetPosition(document, start, span.length)),
             newText,
         }
     })
-}
