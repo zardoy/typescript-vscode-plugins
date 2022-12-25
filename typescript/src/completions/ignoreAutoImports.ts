fe9bc4b4eba4fca703d721334ea1627f03da736a
Vitaly Turovsky <vital2580@icloud.com>
2022-12-25 16:55:32 +0300

    fix(completions-regression): fix autoImport hiding & sorting wasnt working in some (non-global) contexts chore: dev: fix not starting on macOS

--- typescript/src/completions/ignoreAutoImports.ts
+++ typescript/src/completions/ignoreAutoImports.ts
@@ -6,8 +6,9 @@ import { sortBy } from 'rambda'
 export default (entries: ts.CompletionEntry[], languageService: ts.LanguageService, c: GetConfig) => {
     const ignoreAutoImportsSetting = getIgnoreAutoImportSetting(c)
 
-    let newEntries = entries.filter(({ sourceDisplay, name }) => {
-        if (!sourceDisplay) return true
+    const ignoreKinds = [ts.ScriptElementKind.warning, ts.ScriptElementKind.string]
+    entries = entries.filter(({ sourceDisplay, name, kind }) => {
+        if (!sourceDisplay || ignoreKinds.includes(kind)) return true
         const targetModule = ts.displayPartsToString(sourceDisplay)
         const toIgnore = isAutoImportEntryShouldBeIgnored(ignoreAutoImportsSetting, targetModule, name)
         return !toIgnore
@@ -15,7 +16,7 @@ export default (entries: ts.CompletionEntry[], languageService: ts.LanguageServi
     // todo I'm not sure of incomplete completion (wasnt tested)
     // todo don't forget to impl glob there
     const handledSymbolNames = new Set<string>()
-    for (const [i, entry] of newEntries.entries()) {
+    for (const [i, entry] of entries.entries()) {
         const { name } = entry
         if (!entry.sourceDisplay || handledSymbolNames.has(name)) continue
         if (!shouldChangeSortingOfAutoImport(name, c)) continue
@@ -23,7 +24,7 @@ export default (entries: ts.CompletionEntry[], languageService: ts.LanguageServi
         const sortFn = changeSortingOfAutoImport(c, name)
         // TODO probably should be rewrited
         const entriesToSort: ts.CompletionEntry[] = []
-        newEntries = newEntries.filter((entry, k) => {
+        entries = entries.filter((entry, k) => {
             if (k < i) return true
             if (entry.sourceDisplay && entry.name === name) {
                 entriesToSort.push(entry)
@@ -33,7 +34,7 @@ export default (entries: ts.CompletionEntry[], languageService: ts.LanguageServi
         })
         // todo rewrite outer that for loop to index based and increment here on insert length + handledSymbolNames can be removed in that case
         // final one seems to be slow, e.g. it might be slowing down completions
-        newEntries.splice(i, 0, ...sortBy(({ sourceDisplay }) => sortFn(ts.displayPartsToString(sourceDisplay)), entriesToSort))
+        entries.splice(i, 0, ...sortBy(({ sourceDisplay }) => sortFn(ts.displayPartsToString(sourceDisplay)), entriesToSort))
     }
-    return newEntries
+    return entries
 }
