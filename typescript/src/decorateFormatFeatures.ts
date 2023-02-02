27648352b7dd7ba6afdb88b6242a1b6f9ccf03ee
Vitaly Turovsky <vital2580@icloud.com>
2023-02-02 17:35:31 +0300

    fix: allow to have space before ignore format directive and any content after

--- typescript/src/decorateFormatFeatures.ts
+++ typescript/src/decorateFormatFeatures.ts
@@ -2,43 +2,41 @@ import { GetConfig } from './types'
 import { patchMethod } from './utils'
 
 export default (proxy: ts.LanguageService, languageService: ts.LanguageService, c: GetConfig) => {
-    // const oldGetAllRules = tsFull.formatting.getAllRules;
-    // tsFull.formatting.getAllRules = () => {
-    // }
+    // todo: add our formatting rules!
+    // tsFull.formatting.getAllRules
 
+    const isExpectedDirective = (line: string | undefined, expected: string) => {
+        if (!line) return false
+        line = line.trim()
+        if (!line.startsWith('//')) return false
+        line = line.slice(2).trim()
+        if (line.startsWith(expected)) return true
+        return false
+    }
     const isFormattingLineIgnored = (sourceFile: ts.SourceFile, position: number) => {
-        // const sourceFile = languageService.getProgram()!.getSourceFile(fileName)!
         const fullText = sourceFile.getFullText()
         // check that lines before line are not ignored
         const linesBefore = fullText.slice(0, position).split('\n')
-        if (linesBefore[linesBefore.length - 2]?.trim() === '//@ts-format-ignore-line') {
+        if (isExpectedDirective(linesBefore[linesBefore.length - 2], '@ts-format-ignore-line')) {
             return true
         }
 
         let isInsideIgnoredRegion = false
         for (const line of linesBefore) {
-            if (line.trim() === '//@ts-format-ignore-region') {
+            if (isExpectedDirective(line, '@ts-format-ignore-region')) {
                 isInsideIgnoredRegion = true
             }
-            if (line.trim() === '//@ts-format-ignore-endregion') {
+            if (isExpectedDirective(line, '@ts-format-ignore-endregion')) {
                 isInsideIgnoredRegion = false
             }
         }
         return isInsideIgnoredRegion
     }
-    // proxy.getFormattingEditsAfterKeystroke = (fileName, position, key, options) => {
-    //     // if (isFormattingLineIgnored(fileName, position)) {
-    //     //     return []
-    //     // }
-    //     return languageService.getFormattingEditsAfterKeystroke(fileName, position, key, options)
-    // }
-    // proxy.getFormattingEditsForDocument = (fileName, options) => {
-    //     return []
-    // }
     const toPatchFormatMethods = ['formatSelection', 'formatOnOpeningCurly', 'formatOnClosingCurly', 'formatOnSemicolon', 'formatOnEnter']
     for (const toPatchFormatMethod of toPatchFormatMethods) {
         patchMethod(tsFull.formatting, toPatchFormatMethod as any, oldFn => (...args) => {
             const result = oldFn(...args)
+            // arg position depends on the method, so we need to find it
             const sourceFile = args.find(arg => ts.isSourceFile(arg as any))
             return result.filter(({ span }) => {
                 if (isFormattingLineIgnored(sourceFile as ts.SourceFile, span.start)) {
@@ -48,12 +46,6 @@ export default (proxy: ts.LanguageService, languageService: ts.LanguageService, 
             })
         })
     }
-    // proxy.getFormattingEditsForRange = (fileName, start, end, options) => {
-    //     return languageService.getFormattingEditsForRange(fileName, start, end, options).filter(({ span }) => {
-    //         if (isFormattingLineIgnored(fileName, span.start)) {
-    //             return false
-    //         }
-    //         return true
-    //     })
-    // }
+    // we could easily patch languageService methods getFormattingEditsForDocument, getFormattingEditsAfterKeystroke and getFormattingEditsForRange
+    // but since formatting happens in syntax server, we don't have access to actual sourceFile, so we can't provide implementation
 }
