a74f8d0997b1e977d2676adff7fdbcc7c917a5c0
Vitaly Turovsky <vital2580@icloud.com>
2023-03-02 14:28:23 +0300

    feat(new-refactoring): Add Split Declaration and Initialization refactoring code action! Activation range is `const` or `let` keyword. fixes #108 also add comparing refactor extensions docs

---
+++ typescript/src/codeActions/custom/splitDeclarationAndInitialization.ts
@@ -0,0 +1,47 @@
+import { getChangesTracker, getIndentFromPos } from '../../utils'
+import { CodeAction } from '../getCodeActions'
+
+export default {
+    name: 'Split Declaration and Initialization',
+    id: 'splitDeclarationAndInitialization',
+    kind: 'refactor.rewrite.split-declaration-and-initialization',
+    tryToApply(sourceFile, position, range, node, formatOptions, languageService, languageServiceHost) {
+        if (range || !node) return
+        if (!ts.isVariableDeclarationList(node) || node.declarations.length !== 1) return
+        const declaration = node.declarations[0]!
+        if (position > declaration.pos || !declaration.initializer || !ts.isIdentifier(declaration.name)) return
+        if (!formatOptions) return true
+        const typeChecker = languageService.getProgram()!.getTypeChecker()!
+        let typeNode = declaration.type
+        if (!typeNode) {
+            let type = typeChecker.getTypeAtLocation(declaration)
+            if (type.isLiteral()) {
+                type = typeChecker.getBaseTypeOfLiteralType(type)
+            }
+            typeNode = typeChecker.typeToTypeNode(type, node.parent, ts.NodeBuilderFlags.NoTruncation)
+        }
+        const changesTracker = getChangesTracker(formatOptions)
+        const { factory } = ts
+        const nodeStart = node.pos + node.getLeadingTriviaWidth()
+        const varName = declaration.name.text
+        changesTracker.insertNodeAt(
+            sourceFile,
+            nodeStart,
+            factory.createVariableDeclarationList(
+                [factory.createVariableDeclaration(factory.createIdentifier(varName), undefined, typeNode)],
+                ts.NodeFlags.Let,
+            ),
+        )
+        changesTracker.replaceNode(
+            sourceFile,
+            node,
+            factory.createBinaryExpression(factory.createIdentifier(varName), factory.createToken(ts.SyntaxKind.EqualsToken), declaration.initializer),
+            {
+                prefix: `\n${getIndentFromPos(ts, sourceFile, position)}`,
+                suffix: '\n',
+                leadingTriviaOption: /*Ignore all*/ 0,
+            },
+        )
+        return changesTracker.getChanges()[0]?.textChanges as ts.TextChange[]
+    },
+} satisfies CodeAction
