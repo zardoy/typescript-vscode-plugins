f5cfb0905b74baf10eeb31944ca76fe791606bd6
Vitaly Turovsky <vital2580@icloud.com>
2023-03-17 16:59:07 +0300

    method-snippets: don't activate in `typeof`

--- typescript/test/completions.spec.ts
+++ typescript/test/completions.spec.ts
@@ -123,6 +123,7 @@ describe('Method snippets', () => {
 
             declare const a: A
             a/*2*/
+            type B1 = typeof a/*7*/;
 
             // overload
             function foo(this: {}, a)
@@ -169,6 +170,7 @@ describe('Method snippets', () => {
         compareMethodSnippetAgainstMarker(markers, 4, '($b)')
         compareMethodSnippetAgainstMarker(markers, 5, '(a, b, { d, e: {} }, ...c)')
         compareMethodSnippetAgainstMarker(markers, 6, '(a, b, c)')
+        compareMethodSnippetAgainstMarker(markers, 7, null)
     })
 
     test('Skip trailing void', () => {
