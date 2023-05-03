30384a0f42408811a5d31450cadd1265d9d3b1a2
Vitaly Turovsky <vital2580@icloud.com>
2023-05-03 12:11:52 +0300

    fix: update support for new volar

--- src/vueVolarSupport.ts
+++ src/vueVolarSupport.ts
@@ -6,15 +6,12 @@ export default () => {
     if (process.env.PLATFORM !== 'node') return
     const handler = () => {
         const config = vscode.workspace.getConfiguration('')
-        if (
-            !getExtensionSetting('enableVueSupport') ||
-            !vscode.extensions.getExtension('Vue.volar') ||
-            isConfigValueChanged('volar.vueserver.configFilePath')
-        ) {
+        const VOLAR_CONFIG_FILE_SETTING = 'vue.server.configFilePath'
+        if (!getExtensionSetting('enableVueSupport') || !vscode.extensions.getExtension('Vue.volar') || isConfigValueChanged(VOLAR_CONFIG_FILE_SETTING)) {
             return
         }
 
-        void config.update('volar.vueserver.configFilePath', extensionCtx.asAbsolutePath('./volarConfig.js'), vscode.ConfigurationTarget.Global)
+        void config.update(VOLAR_CONFIG_FILE_SETTING, extensionCtx.asAbsolutePath('./volarConfig.js'), vscode.ConfigurationTarget.Global)
     }
 
     handler()
@@ -22,11 +19,11 @@ export default () => {
     vscode.extensions.onDidChange(handler)
 }
 
-const isConfigValueChanged = (id: string) => {
+const isConfigValueChanged = (settingId: string) => {
     if (process.env.PLATFORM !== 'web') {
         const config = vscode.workspace.getConfiguration('')
-        const userValue = config.get<string>(id)
-        if (userValue === config.inspect(id)!.defaultValue) return false
+        const userValue = config.get<string>(settingId)
+        if (userValue === config.inspect(settingId)!.defaultValue) return false
         // means that value was set by us programmatically, let's update it
         // eslint-disable-next-line @typescript-eslint/no-require-imports
         if (userValue?.startsWith(require('path').join(extensionCtx.extensionPath, '../..'))) return false
