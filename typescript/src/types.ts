a9b729ccdeb6c96d65fba24e6614d39cb05c8c43
Vitaly Turovsky <vital2580@icloud.com>
2023-07-19 00:06:22 +0300

    fix build

--- typescript/src/types.ts
+++ typescript/src/types.ts
@@ -4,7 +4,8 @@ import { ConditionalPick } from 'type-fest'
 import type { Configuration } from '../../src/configurationType'
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Configuration = Configuration & { editorSuggestInsertModeReplace: boolean }
-export type GetConfig = <T extends keyof Configuration>(key: T) => Configuration[T]
+type LocalConfig = Configuration & { editorSuggestInsertModeReplace: boolean }
+export type GetConfig = <T extends keyof LocalConfig>(key: T) => LocalConfig[T]
 export type LanguageServiceMethodWithConfig<T extends keyof ConditionalPick<ts.LanguageService, (...args) => any>> = (
     c: GetConfig,
     ...args: Parameters<ts.LanguageService[T]>
