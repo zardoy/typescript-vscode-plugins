da987206c7c47b1d46f6756de20ae15e9d356adb
Ilya Golovin <me@ilscc.ru>
2023-07-21 00:01:45 +0300

    feat(asSuggestions): add asSuggestions

--- typescript/src/completions/sharedContext.ts
+++ typescript/src/completions/sharedContext.ts
@@ -14,5 +14,6 @@ export const sharedCompletionContext = {} as unknown as Readonly<{
     c: GetConfig
     formatOptions: ts.FormatCodeSettings
     preferences: ts.UserPreferences
+    fullText: string
     // languageServiceHost: ts.LanguageServiceHost
 }>
