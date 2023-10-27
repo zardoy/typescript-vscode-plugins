654477becf243258b6b597ee47abccee1ed8bd6e
Vitaly Turovsky <vital2580@icloud.com>
2023-10-27 23:05:32 +0300

    wip2

--- src/onEnterActions.ts
+++ src/onEnterActions.ts
@@ -1,3 +1,15 @@
+import { defaultJsSupersetLangsWithVue } from '@zardoy/vscode-utils/build/langs'
+import * as vscode from 'vscode'
 export default () => {
-    // todo add
+    vscode.workspace.onDidChangeTextDocument(({ contentChanges, document, reason }) => {
+        if (
+            !contentChanges.length ||
+            !vscode.languages.match(defaultJsSupersetLangsWithVue, document) ||
+            vscode.workspace.fs.isWritableFileSystem(document.uri.scheme) === false
+        )
+            return
+        if (document.languageId === 'vue') return // todo
+        const importRegex = /^\s*import\((['"].*['"])\) from (['"].*['"])$/
+        // const prevLine = 
+    })
 }
