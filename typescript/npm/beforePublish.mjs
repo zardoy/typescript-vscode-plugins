aba470cf922e03df5f4e45af6cce9b84643dcb15
Vitaly Turovsky <vital2580@icloud.com>
2024-02-15 20:11:24 +0300

    npm: dont skip build step!

--- typescript/npm/beforePublish.mjs
+++ typescript/npm/beforePublish.mjs
@@ -4,4 +4,5 @@ fs.copyFileSync('./typescript/npm/package.json', './typescript/package.json')
 
 const packageJson = JSON.parse(fs.readFileSync('./typescript/package.json', 'utf8'))
 packageJson.version = JSON.parse(fs.readFileSync('./package.json', 'utf8')).version
+if (packageJson.version === '0.0.0-dev') packageJson.version = '0.0.0'
 fs.writeFileSync('./typescript/package.json', JSON.stringify(packageJson, null, 2))
